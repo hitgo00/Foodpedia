9f6376f08768d32aece838985da853d7641a3b89
hitg00 <hitgo17@gmail.com>
2021-03-19 12:24:00 +0530

    feat: adds Navabr
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { Router } from '@reach/router';
-import './App.css';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import ProfilePage from './pages/ProfilePage';
+import { Router } from "@reach/router";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-     <Router primary={false}>
-          <HomePage path="/" />
-          <SearchPage path="/search" />
-          <ProfilePage path="/profile" />
-        </Router>
+      <Navbar />
+      <Router primary={false}>
+        <HomePage path="/" />
+        <SearchPage path="/search" />
+        <ProfilePage path="/profile" />
+      </Router>
     </div>
   );
 }
---
+++ src/components/navbar/index.js
@@ -0,0 +1,40 @@
+import { Link } from "@reach/router";
+
+const pages = [
+  {
+    label: "Search Page",
+    link: "/search",
+  },
+  {
+    label: "Home Page",
+    link: "/",
+  },
+  {
+    label: "Prfile",
+    link: "/profile",
+  },
+];
+
+function Navbar() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        background: "#0D56FE",
+        height: "3rem",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      {pages.map((page) => (
+        <Link to={page.link} key={page.label}>
+          <div style={{ padding: "8px" }}> {page.label}</div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Navbar;
