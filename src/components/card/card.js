d9a40c91159d72fabe1a2cd285a3658ae9ee3956
Divya Vazirani <201801165@daiict.ac.in>
2021-03-21 19:30:40 +0530

    Create card.js
    created card component

---
+++ src/components/card/card.js
@@ -0,0 +1,143 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import "./card.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 450
+  },
+  media: {
+    height: 0,
+    paddingTop: "67.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  }
+}));
+
+const STYLES = ["btn--primary--solid"];
+
+const SIZES = ["btn--medium", "btn--large"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className="card-container">
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image="https://ardo.com/files/attachments/.18687/w1200h630zcCq85_PAELLA_ROYAL_207.jpg"
+        />
+        <CardContent>
+          <div className="card-title">
+            <Typography variant="h5">Paella Dish</Typography>
+          </div>
+          <Typography variant="body1" component="p">
+            <div className="card-body">
+              {" "}
+              Paella is a Spanish rice dish originally from Valencia. Paella is
+              one of the best-known dishes in Spanish cuisine. For this reason,
+              many non-Spaniards view it as Spain's national dish, but Spaniards
+              almost unanimously consider it to be a dish from the Valencian
+              region.{" "}
+            </div>
+          </Typography>
+          <CardActions disableSpacing>
+            <Typography variant="body1" component="p">
+              <p>Caloric Density (kcal) : 431</p>
+            </Typography>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <Typography variant="body1">
+              <ul>
+                <li>Fat:10.4g</li>
+                <li>Sodium:640mg</li>
+                <li>Carbohydrates:35.7g</li>
+                <li>Fiber:2.5g</li>
+                <li>Protien:12.2g</li>
+              </ul>
+            </Typography>
+          </Collapse>
+          <div className="btn">
+            <Button
+              onClick={() => {
+                console.log("You clicked!");
+              }}
+              type="button"
+              buttonStyle="btn--primary--solid"
+              buttonSize="btn--medium"
+            >
+              Add Item
+            </Button>
+            <Button
+              onClick={() => {
+                console.log("You clicked!");
+              }}
+              type="button"
+              buttonStyle="btn--primary--solid"
+              buttonSize="btn--medium"
+            >
+              Share
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
