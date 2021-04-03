f9fbe25369e8962b6ff66704aa7ac80e4f435782
hitg00 <hitgo17@gmail.com>
2021-04-03 06:03:56 +0530

    fix: home page styles, formatted files
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,30 +6,15 @@ import SearchPage from "./pages/SearchPage";
 import ProfilePage from "./pages/ProfilePage";
 import Navbar from "./components/navbar";
 
-
-
-import {GoogleLogin} from "react-google-login";
-
-const responseGoogle=response=>{
-  console.log(response)
-}
-
 function App() {
   return (
     <div className="App">
-      
-     <Navbar />
+      <Navbar />
       <Router primary={false}>
         <HomePage path="/" />
-       
         <SearchPage path="/search" />
         <ProfilePage path="/profile" />
-      </Router> 
-      <GoogleLogin
-    clientId="850634064633-r2q201m4thuruahn6fc82nl21o0b14dl.apps.googleusercontent.com"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    />
+      </Router>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,27 @@
-import React, { Component } from "react";
-import "./home.css";
-
+import React from "react";
+import { GoogleLogin } from "react-google-login";
 import index from "../index.png";
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
+
 export default function HomePage() {
   return (
-    <div align="centre">
-      <img src={index} class="center"></img>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <img alt="logo" src={index} />
+      <GoogleLogin
+        clientId="850634064633-r2q201m4thuruahn6fc82nl21o0b14dl.apps.googleusercontent.com"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
     </div>
   );
 }
