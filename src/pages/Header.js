a1a64534d969b305c041645e8a04980d576c8d58
hitg00 <hitgo17@gmail.com>
2021-04-17 14:19:37 +0530

    chore: rm unused import
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 import { GoogleLogin } from "react-google-login";
-import index from "../images/index.png";
-import './home.css'
+import "./home.css";
 
 const responseGoogle = (response) => {
-    console.log(response);
+  console.log(response);
 };
 
-const Header =  function Header() {
-    return (
-        <div className="banner">
-        {/* <img alt="logo" src={index} /> */}
-        <GoogleLogin className="login"
-            clientId="850634064633-r2q201m4thuruahn6fc82nl21o0b14dl.apps.googleusercontent.com"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-        />
-        <div className="title">
-            <h3>FoodPedia</h3>
-            <h4>Count Memories Not Calories</h4>
-        </div>
+const Header = function Header() {
+  return (
+    <div className="banner">
+      {/* <img alt="logo" src={index} /> */}
+      <GoogleLogin
+        className="login"
+        clientId="850634064633-r2q201m4thuruahn6fc82nl21o0b14dl.apps.googleusercontent.com"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+      <div className="title">
+        <h3>FoodPedia</h3>
+        <h4>Count Memories Not Calories</h4>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Header;
