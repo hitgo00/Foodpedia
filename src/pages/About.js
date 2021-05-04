9519f413fa46246ac5e65462c78cc949dbfc7444
hemang11 <hemangshrimali22@gmail.com>
2021-05-04 20:00:20 +0530

    chore:Landing page refractored

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import img from "../images/index.png";
-
+import img from "../images/about.png";
+import img2 from "../images/about2.png";
 export default function About() {
     return (
         <div id="about">
             <div className="about-text">
+            <div className="about2">
+                <img src={img2} alt=''/>
+            </div>
                 <h1>About Us</h1>
                 <p>
                     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { GoogleLogin } from "react-google-login";
-import index from "../images/index.png";
+import index from "../images/banner.png";
+import bg from "../images/bg.png"
 import './home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithubAlt,faFacebook,faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 const responseGoogle = (response) => {
     console.log(response);
@@ -9,17 +12,33 @@ const responseGoogle = (response) => {
 
 const Header =  function Header() {
     return (
+        <div className="super">
         <div className="banner">
-        {/* <img alt="logo" src={index} /> */}
-        <GoogleLogin className="login"
+        <div className="banner-text">
+            <h1> We're always in the <br/> mood for food</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi ab minima 
+                excepturi sint libero temporibus numquam aspernatur deleniti quia. 
+                Voluptas architecto incidunt, quaerat dolorum vel.</p>
+            <a href="/search" class="btn">Search Food </a>
+            <GoogleLogin className="login"
             clientId="850634064633-r2q201m4thuruahn6fc82nl21o0b14dl.apps.googleusercontent.com"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
         />
-        <div className="title">
-            <h3>FoodPedia</h3>
-            <h4>Count Memories Not Calories</h4>
         </div>
+            <div className="banner-img">
+                <img src={index} alt="logo"/>
+            </div>
+        </div>
+    
+        <div className="social-media">
+        <ul>
+            <li><a href="#"><FontAwesomeIcon className="icon" icon={faTwitter} /></a></li>
+            <li><a href="#"><FontAwesomeIcon className="icon" icon={faFacebook} /></a></li>
+            <li><a href="https://github.com/hitgo00/2-Foodpedia"><FontAwesomeIcon className="icon" icon={faGithubAlt} /></a></li>
+        </ul>
+    </div>
+    <div className="bg"><img src={bg} alt=""/></div>
     </div>
     )
 }
