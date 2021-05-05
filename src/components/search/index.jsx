8826cb4d3bb5426cd8287225b2bc75f4180076f7
akshyagarg0301 <akshyagarg3389@gmail.com>
2021-05-05 14:57:06 +0530

    resolved bugs

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import "./search.css";
 import { makeStyles } from '@material-ui/core/styles';
 
-import Card from "./card.js"
+import Card from "../card/index"
 const useStyles = makeStyles({
   root: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
