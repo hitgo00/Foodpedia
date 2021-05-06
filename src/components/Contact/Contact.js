4ac46489dda02d71bc80e0dd65daffa085e8f305
Hitesh Goyal <41156157+hitgo00@users.noreply.github.com>
2021-05-06 22:18:36 +0530

    feat: integrates user review form w api (#37)
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,64 @@
-import React from 'react'
-import '../home.css';
+import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import "../home.css";
+import axios from "axios";
 
 export default function Contact() {
-    return (
-        <div id="contact">
-        <h1>Share Your Experience</h1>
-        <form>
-         <input type="text" placeholder="Full Name" />
-            <input type="email" placeholder="type your email"/>
-            <textarea placeholder="description..."></textarea>
-            <input type="submit" value="Submit"/>
-        </form>
-        </div>
-    )
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div id="contact">
+      {isSubmitted ? (
+        <Typography color="inherit" variant="h1">
+          Thanks for your review!
+        </Typography>
+      ) : (
+        <>
+          <h1>Share Your Experience</h1>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              if (name && description)
+                axios
+                  .post("https://foodpedia2.herokuapp.com/addReview", {
+                    name,
+                    email,
+                    description,
+                  })
+                  .then((response) => {
+                    console.log(response);
+                    setIsSubmitted(true);
+                  });
+            }}
+          >
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              type="text"
+              name="name"
+              placeholder="Full Name"
+            />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              name="email"
+              placeholder="type your email"
+            />
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              name="description"
+              placeholder="description..."
+            ></textarea>
+            <input type="submit" value="Submit" />
+          </form>
+        </>
+      )}
+    </div>
+  );
 }
